ddc47e13fe1e26107363f42f3c2894d4593a394b
Volkov Aleksandr <sanya005volkov@mail.ru>
2026-02-25 00:05:20 +0300

    solve task_2

--- playground/solve_first_3_tasks.py
+++ playground/solve_first_3_tasks.py
@@ -9,3 +9,11 @@ def first_multiple_index(a: List[int], M: int) -> int:
         if x % M == 0:
             return i
     return -1
+
+
+# Индекс последнего элемента массива, для которого |a[i]| > T.
+def last_abs_gt_index(a: List[int], T: int) -> int:
+    for i in range(len(a) - 1, -1, -1):
+        if abs(a[i]) > T:
+            return i
+    return -1
